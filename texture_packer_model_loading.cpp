98e55dddec181d4e160c50f6d14d7c50b4d74fbe
cuppajoeman <ccn@cuppajoeman.com>
2025-02-15 19:58:18 -0500

    fix: namespaces

--- texture_packer_model_loading.cpp
+++ texture_packer_model_loading.cpp
@@ -16,8 +16,9 @@ std::vector<IVPNTPRigged> convert_ivpnt_to_ivpntpr(std::vector<IVPNTRigged> &ivp
     return ivpntprs;
 }
 
-std::vector<IVPTexturePacked> convert_ivpt_to_ivptp(std::vector<IVPTextured> &ivpts, TexturePacker &texture_packer) {
-    std::vector<IVPTexturePacked> ivptps = {};
+std::vector<draw_info::IVPTexturePacked> convert_ivpt_to_ivptp(std::vector<draw_info::IVPTextured> &ivpts,
+                                                               TexturePacker &texture_packer) {
+    std::vector<draw_info::IVPTexturePacked> ivptps = {};
     for (auto &ivpt : ivpts) {
         std::vector<glm::vec2> packed_texture_coordinates;
         for (auto &tc : ivpt.texture_coordinates) {
@@ -27,25 +28,26 @@ std::vector<IVPTexturePacked> convert_ivpt_to_ivptp(std::vector<IVPTextured> &iv
         int packed_texture_index = texture_packer.get_packed_texture_index_of_texture(ivpt.texture_path);
         int texture_bounding_box_index =
             texture_packer.get_packed_texture_bounding_box_index_of_texture(ivpt.texture_path);
-        IVPTexturePacked ivptp(ivpt.indices, ivpt.xyz_positions, ivpt.texture_coordinates, packed_texture_coordinates,
-                               packed_texture_index, texture_bounding_box_index, ivpt.texture_path);
+        draw_info::IVPTexturePacked ivptp(ivpt.indices, ivpt.xyz_positions, ivpt.texture_coordinates,
+                                          packed_texture_coordinates, packed_texture_index, texture_bounding_box_index,
+                                          ivpt.texture_path);
         ivptps.push_back(ivptp);
     }
     return ivptps;
 }
 
-std::vector<IVPNTexturePacked> convert_ivpnt_to_ivpntp(std::vector<IVPNTextured> &ivpnts,
-                                                       TexturePacker &texture_packer) {
-    std::vector<IVPNTexturePacked> ivpntps = {};
+std::vector<draw_info::IVPNTexturePacked> convert_ivpnt_to_ivpntp(std::vector<draw_info::IVPNTextured> &ivpnts,
+                                                                  TexturePacker &texture_packer) {
+    std::vector<draw_info::IVPNTexturePacked> ivpntps = {};
     for (auto &ivpnt : ivpnts) {
         std::vector<glm::vec2> packed_texture_coordinates =
             texture_packer.get_packed_texture_coordinates(ivpnt.texture_path, ivpnt.texture_coordinates);
         int packed_texture_index = texture_packer.get_packed_texture_index_of_texture(ivpnt.texture_path);
         int texture_bounding_box_index =
             texture_packer.get_packed_texture_bounding_box_index_of_texture(ivpnt.texture_path);
-        IVPNTexturePacked ivpntp(ivpnt.indices, ivpnt.xyz_positions, ivpnt.normals, ivpnt.texture_coordinates,
-                                 packed_texture_coordinates, packed_texture_index, texture_bounding_box_index,
-                                 ivpnt.texture_path);
+        draw_info::IVPNTexturePacked ivpntp(ivpnt.indices, ivpnt.xyz_positions, ivpnt.normals,
+                                            ivpnt.texture_coordinates, packed_texture_coordinates, packed_texture_index,
+                                            texture_bounding_box_index, ivpnt.texture_path);
         ivpntps.push_back(ivpntp);
     }
     return ivpntps;
--- texture_packer_model_loading.hpp
+++ texture_packer_model_loading.hpp
@@ -4,8 +4,9 @@
 #include "sbpt_generated_includes.hpp"
 
 std::vector<IVPNTPRigged> convert_ivpnt_to_ivpntpr(std::vector<IVPNTRigged> &ivptrs, TexturePacker &texture_packer);
-std::vector<IVPTexturePacked> convert_ivpt_to_ivptp(std::vector<IVPTextured> &ivptrs, TexturePacker &texture_packer);
-std::vector<IVPNTexturePacked> convert_ivpnt_to_ivpntp(std::vector<IVPNTextured> &ivptrs,
-                                                       TexturePacker &texture_packer);
+std::vector<draw_info::IVPTexturePacked> convert_ivpt_to_ivptp(std::vector<draw_info::IVPTextured> &ivptrs,
+                                                               TexturePacker &texture_packer);
+std::vector<draw_info::IVPNTexturePacked> convert_ivpnt_to_ivpntp(std::vector<draw_info::IVPNTextured> &ivptrs,
+                                                                  TexturePacker &texture_packer);
 
 #endif // TEXTURE_PACKER_MODEL_LOADING_HPP
