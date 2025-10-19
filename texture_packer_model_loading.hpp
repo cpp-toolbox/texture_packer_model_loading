3fbc3ff4253ab68cfd6249a9f9007b1dfc6c96ce
ccn <ccn@cuppajoeman.com>
2025-10-18 22:38:44 -0400

    docs: add docstring to main function, update sbpt dependencies

--- texture_packer_model_loading.hpp
+++ texture_packer_model_loading.hpp
@@ -12,6 +12,16 @@ std::vector<draw_info::IVPTexturePacked> convert_ivpt_to_ivptp(std::vector<draw_
 std::vector<draw_info::IVPNTexturePacked> convert_ivpnt_to_ivpntp(std::vector<draw_info::IVPNTextured> &ivptrs,
                                                                   TexturePacker &texture_packer);
 
+/**
+*
+* @pre assumes that all used textures have already been packed using the texture packer
+*
+* @brief loads in a model with textures with data to integrate into texture packer shaders
+*
+* @todo remove these object id generators, I don't believe they're required anymore because my drawing system
+*will automatically assign an object id of one doesn't have one, but then again I need to think about this because we
+haven't yet figured out the whole dual id problem with the object id vs the ltw object id etc...
+*/
 draw_info::TransformedIVPTPGroup parse_model_into_tig(ResourcePath &rp, const std::string &model_path,
                                                       TexturePacker &texture_packer,
                                                       UniqueIDGenerator &object_id_generator,
